77d3a55051cf0ed70d143bad15825f6caff505f4
menteneon <128399807+menteneon@users.noreply.github.com>
2023-04-04 21:22:29 -0400

    Create menteneon.py

---
+++ menteneon.py
@@ -0,0 +1,4 @@
+import re
+frase = "agregar parrafo"
+nueva_frase = frase.replace('á', '&aacute;').replace('é', '&eacute;').replace('í', '&iacute;').replace('ó', '&oacute;').replace('ú', '&uacute;').replace('ñ', '&ntilde;')
+print (nueva_frase) 
